e3ea992e70b94497e7c0a07aad6b45ca29a82318
Mcdavid Emereuwa <mcdavidemereuwa95@gmail.com>
2017-05-01 17:19:44 +0100

    Add createIndex method

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,1 +1,24 @@
-var mcdavid
+'use stict';
+
+/**
+ * @class InvertedIndex
+ */
+class InvertedIndex {
+/**
+ * @constructor
+ */
+  constructor() {
+    this.indexMap = {};
+  }
+
+  /**
+   *
+   * @param {string}filename of file
+   * @param {Object}filecontent content of file to be indexed
+   * @return {Object}indexed filename and index(indicies)
+   */
+  createIndex(filename, filecontent) {
+
+  }
+}
+module.exports = { InvertedIndex };
